5629eb7a8622bd1bf127f53f5effeaeb8fc7aefa
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-01-13 17:56:15 -0500

    added js models for the Game class and Review class

---
+++ js/models.js
@@ -0,0 +1,43 @@
+class Game {
+    constructor(attributes) {
+      let whitelist = ["id", "name"]
+      whitelist.forEach(attr => this[attr] = attributes[attr])
+    }
+    /*
+    Game.container() returns a reference to this DOM node:
+    <section id="gamesContainer" class="px-4 bg-teal-100 min-h-screen rounded-md shadow">
+      <h1 class="text-2xl semibold border-b-4 border-teal">Game</h1>
+      <ul id="lists" class="list-none">
+      </ul>
+    </section>
+    */
+    static container() {
+      return this.c ||= document.querySelector("#gamesContainer")
+    }
+    /*
+    Game.list() returns a reference to this DOM node:
+    <ul id="lists" class="list-none">
+    </ul>
+    */
+    static list() {
+      return this.l ||= document.querySelector('#lists')
+    }
+  
+    /*
+    <div
+    */
+    render() {
+  
+    }
+  }
+  
+  class Review {
+    constructor(attributes) {
+      let whitelist = ["id", "name", "game_id", "review_post"]
+      whitelist.forEach(attr => this[attr] = attributes[attr])
+    }
+  
+    static container() {
+      return this.c ||= document.querySelector("#reviews")
+    }
+  }
