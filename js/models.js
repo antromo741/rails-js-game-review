f3c91a32a1cfb7e0740295c76d56b414435fd1d9
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-01-14 19:59:48 -0500

    added ability to generate html with data from api in my index

--- js/models.js
+++ js/models.js
@@ -15,16 +15,9 @@ class Game {
     </section>
     */
     static container() {
-      return this.c ||= document.querySelector("#gamesContainer")
+      return this.c ||= document.querySelector('#lists')
     }
-    /*
-    Game.list() returns a reference to this DOM node:
-    <ul id="lists" class="list-none">
-    </ul>
-    */
-    static list() {
-      return this.l ||= document.querySelector('#lists')
-    }
+  
   /*
   Game.all() will return a promise for all of the game objects that we get from fetching the game
   this collection will be stored locally in game.collection so we can refernce it after yhe 
@@ -46,8 +39,9 @@ class Game {
         })
         .then(gameArray => {
           this.collection = gameArray.map(attrs => new Game(attrs))
-          console.log(this);
-            debugger
+          let renderedLists = this.collection.map(game => game.render())
+          this.container().append(...renderedLists);
+            return this.collection
         })
     }
     /*
@@ -55,16 +49,33 @@ class Game {
     */
     render() {
       this.element ||= document.createElement('li')
-    }
+      this.element.classList.add(..."my-2 px-4 bg-red-500 grid grid-cols-12 sm:grid-cols-6".split(" "));
+      
+      this.nameLink ||= document.createElement('a');
+      this.nameLink.classList.add(..."py-4 col-span-10 sm:col-span-4".split(" "));
+      this.nameLink.textContent = this.name;
+     
+      this.editLink ||= document.createElement('a');
+      this.editLink.classList.add(..."my-4 text-right".split(" "));
+      this.editLink.innerHTML = `<i class="fa fa-pencil-alt"></i>`;
+      
+      this.deleteLink ||= document.createElement('a')
+      this.deleteLink.classList.add(..."my-4 text-right".split(" "));
+      this.deleteLink.innerHTML = `<i class="fa fa-trash-alt"></i>`;
+      
+    this.element.append(this.nameLink, this.editLink, this.deleteLink);
+    
+  return this.element
+
   }
-  
-  class Review {
-    constructor(attributes) {
-      let whitelist = ["id", "name", "game_id", "review_post"]
+}
+class Review {
+  constructor(attributes) {
+    let whitelist = ["id", "name", "game_id", "review_post"]
       whitelist.forEach(attr => this[attr] = attributes[attr])
-    }
-  
-    static container() {
-      return this.c ||= document.querySelector("#reviews")
-    }
   }
+  
+  static container() {
+    return this.c ||= document.querySelector("#reviews")
+  }
+}
